92af1b18a3441ccef4afe5f50ef87a6c74366253
Your Name <you@example.com>
2023-02-09 18:28:41 +0700

    t

---
+++ bai tap 1/Class.sql
@@ -0,0 +1,9 @@
+
+create table Class (
+id int(100),
+name varchar(50)
+);
+insert into Class values 
+('1', 'Duy');
+insert into Class values
+('2', 'Huy');
---
+++ bai tap 1/Teacher.sql
@@ -0,0 +1,8 @@
+create table Teacher(
+id int(11) primary key,
+name varchar(50),
+age int (100),
+country varchar(255)
+);
+insert into teacher values
+('1', 'Duy Nguyen', '27','da nang');
